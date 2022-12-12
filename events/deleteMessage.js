100fd2b909d367da740d97673d374113c82b4edf
Masaki Mita <mimimita3131@gmail.com>
2022-12-12 12:04:45 +0900

    Update deleteMessage.js

--- events/deleteMessage.js
+++ events/deleteMessage.js
@@ -4,9 +4,6 @@ module.exports = {
     name: 'messageCreate',
     once: false,
     async execute(client, message) {
-        if (message.author.bot) return false;
-        if (message.mentions.users.size > 0 && !message.mentions.has(client.user)) return false;
-
         if (!await EraseTargetStore.isTarget(message.guildId, message.channelId)) return;
 
         setTimeout(() => {
