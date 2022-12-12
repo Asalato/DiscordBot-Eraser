c6194e9f0eb4d347a6f6e8bad0f4d2a4ebb4e6e3
Masaki Mita <mimimita3131@gmail.com>
2022-12-12 12:39:19 +0900

    フォルダがないときに作成

--- eraseTargetStore.js
+++ eraseTargetStore.js
@@ -1,10 +1,15 @@
 const fs = require('node:fs');
+const path = require("node:path");
 
 let data = null;
 const fileName = './data/.eraseTargetStore';
 
 module.exports = class EraseTargetStore {
     static #save() {
+        const dirName = path.dirname(fileName);
+        if (!fs.existsSync(dirName)) {
+            fs.mkdirSync(dirName, {recursive: true});
+        }
         fs.writeFileSync(fileName, JSON.stringify(data));
     }
 
