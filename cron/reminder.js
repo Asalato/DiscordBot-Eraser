fcee1c5b64b8c4f2d4438c6b5d0e04eaba5f6cf4
Masaki Mita <mimimita3131@gmail.com>
2023-04-05 22:05:10 +0900

    エラーハンドリングの追加

--- cron/reminder.js
+++ cron/reminder.js
@@ -4,29 +4,33 @@ const ReminderStore = require("../reminderStore");
 module.exports = {
     execute(client) {
         cron.schedule('* * * * *', async () => {
-            const now = new Date(new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
-            const newData = [];
-            const data = ReminderStore.getAllData();
+            try {
+                const now = new Date(new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo'}));
+                const newData = [];
+                const data = ReminderStore.getAllData();
 
-            for(let i = 0; i < data.length; ++i) {
-                if (now < data[i].limit) {
-                    newData.push(data[i]);
-                    continue;
+                for (let i = 0; i < data.length; ++i) {
+                    if (now < data[i].limit) {
+                        newData.push(data[i]);
+                        continue;
+                    }
+
+                    const channel = client.channels.cache.get(data[i].channel);
+                    const message = await channel.messages.fetch(data[i].messageId)
+                        .catch(err => console.error(err));
+
+                    const replyMessage = `<@${data[i].target}> **${data[i].desc}** の時間になりました`;
+                    if (message) {
+                        message.reply(replyMessage);
+                    } else {
+                        channel.send(replyMessage);
+                    }
                 }
 
-                const channel = client.channels.cache.get(data[i].channel);
-                const message = await channel.messages.fetch(data[i].messageId)
-                    .catch(err => console.error(err));
-
-                const replyMessage = `<@${data[i].target}> **${data[i].desc}** の時間になりました`;
-                if (message) {
-                    message.reply(replyMessage);
-                } else {
-                    channel.send(replyMessage);
-                }
+                ReminderStore.setAllData(newData)
+            } catch (e) {
+                console.error(e);
             }
-
-            ReminderStore.setAllData(newData)
         }, {scheduled: true, timezone:'Asia/Tokyo'});
     }
 }
